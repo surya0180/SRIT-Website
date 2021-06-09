e1bc9d7384b8663353d615fa035cecded5705c90
bannu0snake <mahaboob.developer@gmail.com>
2021-06-09 17:31:47 +0530

    add some html features

--- SRIT/settings.py
+++ SRIT/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'SRIT',
         'USER': 'postgres',
-        'PASSWORD': 'Surya_0180',
+        'PASSWORD': 'Bannu@123',
         'HOST': 'localhost',
     }
 }
