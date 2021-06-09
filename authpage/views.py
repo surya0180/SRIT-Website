d8f7362ff6faa517b6930185be038ef9d743674f
surya0180 <teja8551@gmail.com>
2021-06-09 16:42:29 +0530

    Added Signup feature

--- authpage/views.py
+++ authpage/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from .models import Student
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -6,4 +8,22 @@ def login(request):
     return render(request, 'login.html')
 
 def signup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        rollnumber = request.POST['rollnumber']
+        password = request.POST['psw']
+
+        if Student.objects.filter(Username = username).exists():
+            return redirect('signup')
+        if Student.objects.filter(RollNumber = rollnumber).exists():
+            return redirect('signup')
+        
+        user = User.objects.create(username=username, password=password)
+        user.is_active = True
+        user.save()
+
+        Student.objects.all().create(Username=username, RollNumber=rollnumber)
+        
+        return redirect('index')
+
     return render(request, 'signup.html')
