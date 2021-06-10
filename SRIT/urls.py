cd494dfbeb93d4ca9f5cbc04cf069ab09c262691
bannu0snake <mahaboob.developer@gmail.com>
2021-06-10 12:28:54 +0530

    added pie chart

--- SRIT/urls.py
+++ SRIT/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django import urls
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('', include('homepage.urls')),
     path('login/', include('authpage.urls')),
     path('dashboard/', include('dashboard.urls')),
     path('admin/', admin.site.urls),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
