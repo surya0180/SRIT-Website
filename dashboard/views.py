c07bcfbea9adc33cdec4fbe5d0fd4f6c55830e06
surya0180 <teja8551@gmail.com>
2021-06-10 15:17:02 +0530

    Added subject wise part in the dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 import random
-# import matplotlib
-# matplotlib.use('Agg')
-# from matplotlib import pyplot as plt
+import matplotlib
+matplotlib.use('Agg')
+from matplotlib import pyplot as plt
 import numpy as np
 from django.core.files.storage import FileSystemStorage
 # Create your views here.
@@ -17,15 +17,15 @@ def activities(request):
     return render(request, 'activities.html')
 
 def subjectWise(request):
-    # labels = 'mid-1', 'mid-2'
-    # sizes = [random.randint(10,30), random.randint(30,50)]
-    # explode = (0.1, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
-    # fig1, ax1 = plt.subplots()
-    # ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
-    #         shadow=True, startangle=90)
-    # ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-    # plt.savefig('media/sale_purchase_peichart.png',dpi=100)
-    
+    labels = 'mid-1', 'mid-2'
+    sizes = [random.randint(10,30), random.randint(30,50)]
+    explode = (0.1, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+    fig1, ax1 = plt.subplots()
+    ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',
+            shadow=True, startangle=90)
+    ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+    plt.savefig('media/sale_purchase_peichart.png',dpi=100)
+    plt.close()
     
     
     return render(request, 'subjectWise.html')
